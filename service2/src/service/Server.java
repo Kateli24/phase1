c06966f412dac90be0da67fbe82e224dfc20cbad
kate <katelee817@outlook.com>
2018-03-19 10:01:38 +1300

    created server for account service. Close #11.

--- service2/src/service/Server.java
+++ service2/src/service/Server.java
@@ -5,10 +5,56 @@
  */
 package service;
 
+import dao.CustomerDAO;
+import domain.Customer;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import org.jooby.Jooby;
+import org.jooby.apitool.ApiTool;
+import org.jooby.json.Gzon;
+import resource.CustomerResource;
+import resource.CustomersResource;
+
 /**
  *
  * @author jiaweili
  */
-public class Server {
+public class Server extends Jooby{
+    public Server() {
+
+		CustomerDAO dao = new CustomerDAO();
+
+		use(new Gzon());
+		use(new CustomerResource(dao));
+		use(new CustomersResource(dao));
+
+		use(new ApiTool()
+				.modify(r -> r.pattern().equals("/api/customers"), route -> {
+
+					// Fix response type since Swagger couldn't guess
+					route.response().type(new Customer[0].getClass());
+
+				})
+				.swagger());
+
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		Server server = new Server();
+                
+                server.port(8082);
+
+		CompletableFuture.runAsync(() -> {
+			server.start();
+		});
+
+		server.onStarted(() -> {
+			System.out.println("\nPress Enter to stop service.");
+		});
+
+		System.in.read();
+		server.stop();
+	}
     
 }
